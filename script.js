84561c09b1029937392c90445e7e24813a114b23
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-11-24 12:34:17 +0530

    Added some more meaningful comments for the script file...

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // This is for capturing the HTML element for further DOM manipulation...
 const toggles = document.querySelectorAll(".faq-toggle");
 
+
+// This is for toggling the buttons...
 toggles.forEach((toggle) => {
   toggle.addEventListener("click", () => {
     toggle.parentNode.classList.toggle("active");
